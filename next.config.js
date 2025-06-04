b5a344d25a84d31281f2bd4405fbb487682456ce
uke_mba <uke_sjtu@sjtu.edu.cn>
2025-06-04 09:08:52 +0800

    Update Next.js configuration and package scripts; enhance next.config.js with output settings and add export script to package.json

--- next.config.js
+++ next.config.js
@@ -1,14 +1,24 @@
-import nextra from 'nextra';
+import nextra from "nextra";
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: "export",
+    images: {
+        unoptimized: true,
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+    trailingSlash: true,
+};
 
 const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-  staticImage: true,
-  defaultShowCopyCode: true,
-  readingTime: true,
+    theme: "nextra-theme-docs",
+    themeConfig: "./theme.config.tsx",
+    staticImage: true,
+    defaultShowCopyCode: true,
+    readingTime: true,
 });
 
-export default withNextra({
-  reactStrictMode: true,
-  swcMinify: true,
-});
+export default withNextra(nextConfig);
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './**/*.mdx'
+    // './**/*.mdx'
   ],
   theme: {
     extend: {
